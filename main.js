07d924335e45c6c8a9f74bf5017233132107ec00
FelipeHdez <felipinhdez@gmail.com>
2020-08-14 19:55:09 -0500

    Add display functionality

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
+let displayValue = '';
+
+const calcKeys = Array.from(document.querySelectorAll('[data-n]'));
+
+calcKeys.forEach( element =>{
+    element.addEventListener('click', updateDisplayValue);
+});
+
+function updateDisplayValue(newValue){
+    if (typeof newValue === 'object'){
+        newValue = newValue.target.getAttribute('data-n');
+    }
+    displayValue += newValue;
+    const display = document.querySelector('#display');
+    console.log('a');
+    display.textContent = displayValue;
+    
+}
+
+
+
 function add (a, b) {
     return a + b;
 }
